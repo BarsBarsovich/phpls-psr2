f8c2d3168e53517038818d041428b3659454017c
Boris <boris.dryabin@gmail.com>
2020-02-20 22:40:51 +0300

    initial commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * @ const
+ */
+CONST TESTCONSTANTA = 'asd';
+$user_name = "Igor";
+
+/**
+ * @ Some desc
+ *
+ * @return string
+ */
+function Show_something()
+{
+    RETURN 'SOMETHING';
+}
+
+if (1 == 1) {
+    echo 'hi';
+}
